9962ff5e2e2ee16fb2c917fdbb350a142c7320da
neojarvis <techadmin@iamneo.ai>
2024-01-30 07:53:24 +0000

    Auto-save-commit-1/30/2024--1:23:22-PM

--- Demo.java
+++ Demo.java
@@ -1,2 +1,7 @@
 console.log("java app")
 console.log("java script app");
+console.log("spring app");
+console.log("angular app");
+console.log("cucumber app");
+console.log("TestNg app");
+console.log("COD APP");
